ed158342e73a107f7f46890a4ff5a93648b3f718
Luan Bodner <luan.rosario@ligue.net>
2019-10-02 15:55:20 -0300

    Basic Routing

--- src/server.js
+++ src/server.js
@@ -1,24 +1,22 @@
 const http = require('http');
 const fs = require('fs');
 
-var myReadStream = fs.createReadStream(__dirname + '/resources/stream.txt', 'utf8');
+var server = http.createServer(function(request, response) {
 
-myReadStream.on('data', function(chunk) {
-  console.log('new chunk received:');
-  console.log(chunk);
+  if (request.url === '/home' || request.url === '/') {
+    response.writeHead(200, {
+      'Content-Type': 'text/html'
+    });
+
+    fs.createReadStream(__dirname + '/resources/read.html').pipe(response);
+  } else if (request.url === '/contact') {
+    response.writeHead(200, {
+      'Content-Type': 'text/html'
+    });
+
+    fs.createReadStream(__dirname + '/resources/contact.html').pipe(response);
+  }
 });
 
-
-
-// var server = http.createServer(function(req, res) {
-//
-//   console.log('request was made')
-//   res.writeHead(200, {
-//     'Content-Type': 'text/plain'
-//   });
-//
-//   res.end('Sup nibbas');
-// });
-//
-// server.listen(3000, '127.0.0.1');
-// console.log('LISTENING');
+server.listen(3000, '127.0.0.1');
+console.log('LISTENING');
